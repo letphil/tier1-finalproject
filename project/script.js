eb0951bfe1058af423a027f4c5d1af21e8caedd9
Choi Phil <philchoi@icloud.com>
2025-03-13 12:17:19 +0900

    j

--- project/script.js
+++ project/script.js
@@ -1,6 +1,8 @@
 function handleAuthButtons() {
   if (localStorage.getItem("user")) {
     authButtons.innerHTML = "<p onclick='logout()'>logout</p>";
+    contentSection.innerHTML = "<h1>SECRET SOCIETY</h1>";
+    callRandomUsers();
   } else {
     authButtons.innerHTML = `
       <button
@@ -26,6 +28,50 @@ function handleAuthButtons() {
 function logout() {
   localStorage.removeItem("user");
   handleAuthButtons();
+  contentSection.innerHTML = `
+          <div id="carouselExample" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                src="https://images.pexels.com/photos/27372369/pexels-photo-27372369/free-photo-of-a-person-holding-a-butterfly-on-their-finger.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="https://images.pexels.com/photos/27372369/pexels-photo-27372369/free-photo-of-a-person-holding-a-butterfly-on-their-finger.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="https://images.pexels.com/photos/27372369/pexels-photo-27372369/free-photo-of-a-person-holding-a-butterfly-on-their-finger.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>`;
 }
 
 /**
@@ -41,6 +87,9 @@ function login(username, password) {
     // set localstorage here
     localStorage.setItem("user", JSON.stringify(attempt));
     handleAuthButtons();
+    // could use document.getElemebyId here but is ok to use camelCased id
+    contentSection.innerHTML = "<h1>SECRET SOCIETY</h1>";
+    callRandomUsers();
   } else {
     alert("WRONG CREDENTIALS");
   }
@@ -52,6 +101,55 @@ loginBtn.addEventListener("click", function () {
   recipientPassword.value = "";
 });
 
+async function callRandomUsers() {
+  const div = document.createElement("div");
+  const ru = await (await fetch("https://randomuser.me/api?results=30")).json();
+  renderRandomUsers(ru.results);
+  // div.innerHTML = `<pre>${JSON.stringify(ru, null, 2)}</pre>`;
+  // contentSection.appendChild(div);
+}
+
 window.onload = () => {
   handleAuthButtons();
 };
+
+function renderRandomUsers(data) {
+  for (let i = 0; i < data.length; i++) {
+    const current = data[i];
+    console.log("current =", current);
+    const currentImage = current.picture.medium;
+    const currentName = `${current.name.title} ${current.name.first} ${current.name.last}`;
+    const currentLocation = `${current.location.city}, ${current.location.country}`;
+    const currentEmail = current.email;
+    const currentPhone = current.phone;
+    const randomUserCard = RandomUserCard(
+      currentImage,
+      currentName,
+      currentLocation,
+      currentEmail,
+      currentPhone
+    );
+    contentSection.innerHTML += randomUserCard;
+  }
+}
+
+function RandomUserCard(img, name, location, email, phone) {
+  return `
+    <div class="random-user-card">
+      <div>
+        <img
+          class="profile-pic"
+          src="${img}"
+        />
+      </div>
+      <div>
+        <p>${name}</p>
+        <p>${location}</p>
+      </div>
+      <div class="action-buttons-container">
+        <a href="mailto:${email}"><i class="fa-solid fa-envelope"></i>email</a>
+        <a href="tel:${phone}"><i class="fa-solid fa-phone"></i>phone</a>
+      </div>
+    </div>
+  `;
+}
